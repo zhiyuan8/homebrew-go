950ccc5ba0d3af4d4b4708974c63fe6a2cc57981
lxsong <672528307@qq.com>
2025-07-08 01:18:11 +0800

    update formula

--- Formula/nexa.rb
+++ Formula/nexa.rb
@@ -39,39 +39,23 @@ class Nexa < Formula
     end
   end
 
-  def install
-    # The tarball was created with `-C`, so all files are at the root.
-    bin.install "nexa"
-    bin.install "nexa-cli"
-    libexec.install "lib"
-  end
+def install
+      libexec.install "nexa"
+      libexec.install "nexa-cli"
+      libexec.install "lib"
 
-  def post_install
-    # This block creates wrapper scripts to set DYLD_LIBRARY_PATH
-    # It moves the original executables and replaces them with the wrappers
-
-    # For nexa executable
-    if (bin/"nexa").exist? && !(bin/"nexa").symlink?
-      (bin/"nexa.original").unlink if (bin/"nexa.original").exist?
-      mv bin/"nexa", bin/"nexa.original"
       (bin/"nexa").write <<~EOS
         #!/bin/bash
         export DYLD_LIBRARY_PATH="#{libexec}/lib"
-        exec "#{bin}/nexa.original" "$@"
+        exec "#{libexec}/nexa" "$@"
       EOS
-    end
 
-    # For nexa-cli executable
-    if (bin/"nexa-cli").exist? && !(bin/"nexa-cli").symlink?
-      (bin/"nexa-cli.original").unlink if (bin/"nexa-cli.original").exist?
-      mv bin/"nexa-cli", bin/"nexa-cli.original"
       (bin/"nexa-cli").write <<~EOS
         #!/bin/bash
         export DYLD_LIBRARY_PATH="#{libexec}/lib"
-        exec "#{bin}/nexa-cli.original" "$@"
+        exec "#{libexec}/nexa-cli" "$@"
       EOS
     end
-  end
 
   test do
     assert_match "version", shell_output("#{bin}/nexa-cli --version")
